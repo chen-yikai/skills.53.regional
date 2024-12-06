589ffb45956e9ed8d25b8e8da490302e3ded8ffa
chen_yikai <s11304127@school.saihs.edu.tw>
2024-12-06 20:39:24 +0800

    add splash screen image

--- app/src/main/java/com/example/skills53dic/MainActivity.kt
+++ app/src/main/java/com/example/skills53dic/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.skills53dic
 
 import android.os.Bundle
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.skills53dic.ui.theme.Skills53dicTheme
 
@@ -25,6 +33,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainView(){
+fun NavHoster() {
 
+}
+
+@Composable
+fun SplashScreen() {
+    var showSplashScreen by rememberSaveable { mutableStateOf(true) }
+    if (showSplashScreen) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(R.drawable.splash_screen),
+                contentDescription = "Splash Screen"
+            )
+        }
+
+    } else {
+        NavHoster()
+    }
 }
