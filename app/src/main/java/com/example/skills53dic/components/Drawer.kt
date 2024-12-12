96b475a7283698030380dbcc443fbf40c11f8603
eliaschen <eliaschen.dev+chen_yikai@gmail.com>
2024-12-12 15:08:45 +0800

    add navlinks json

--- app/src/main/java/com/example/skills53dic/components/Drawer.kt
+++ app/src/main/java/com/example/skills53dic/components/Drawer.kt
@@ -1,5 +1,6 @@
 package com.example.skills53dic.components
 
+import android.icu.text.CaseMap.Title
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
@@ -9,19 +10,34 @@ import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.DrawerState
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.google.gson.Gson
+
+data class SubNavLinksList(
+    val title: String,
+    val route: String,
+)
+
+data class NavLinksList(
+    val title: String,
+    val route: String,
+    val sub: List<NavLinksList> = emptyList()
+)
 
 @Composable
 fun DrawerContent(drawerState: DrawerState) {
+    val context = LocalContext.current
+    val navLinksJson = context.assets.open("nav_links.json")
+        .bufferedReader()
+        .use { it.readText() }
+    val navLinks = Gson().fromJson(navLinksJson, Array<NavLinksList>::class.java).toList()
     Column(
         modifier = Modifier
             .fillMaxHeight()
@@ -31,6 +47,19 @@ fun DrawerContent(drawerState: DrawerState) {
             .padding(horizontal = 10.dp, vertical = 20.dp)
             .padding(start = 20.dp)
     ) {
-        Text("Drawer")
+        navLinks.forEach { navlinks ->
+            Column {
+                TextButton(onClick = {}) {
+                    Text("${navlinks.title}")
+                }
+                if (navlinks.sub.isNotEmpty()) {
+                    Column {
+                        navlinks.sub.forEach { subitem ->
+                            Text("${subitem.title}")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
